bdd96ab1b51246b10c15a02e808427da46467a3c
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-03-11 21:39:37 -0300

    !

---
+++ Estudo-FormularioWeb/exerciocio_id_class.js
@@ -0,0 +1,8 @@
+document.write("Ola Mundo")
+document.write(100+"<br>")
+document.write("40"+200+"<br>")
+document.write(30+43+"<br>")
+
+document.write("Ola Mundo<br>Hoje é JS")
+
+document.write("<p style='color:#f00'>Ola Mundo<br> exemplo de paragrafo</p>")
