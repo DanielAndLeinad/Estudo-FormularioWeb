ad57eed24af9f08f587580d01e7a92c5df5eac97
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-03-18 21:09:11 -0300

    !

--- alertALERT/script.js
+++ alertALERT/script.js
@@ -2,8 +2,8 @@ function coletInf(){
     let nome = prompt("Qual seu nome?")
     let idade = prompt("Qual sua idade?")
 
-    document.getElementById("mensagem").innerHTML = "Olá" + nome + "! você tem" + idade + " anos"
-    if (idadew < 18){
+    document.getElementById("mensagem").innerHTML = "Olá" + nome + "! você tem " + idade + " anos"
+    if (idade < 18){
         alert("voce é de menor")
     } else{
         alert("voce é de maior")
